4331b002b5d14e45a3d4da5e541370e3a0876bf1
Nils Knappmeier <n.knappmeier@i-views.de>
2016-01-16 08:48:46 +0100

    Initial check-in

---
+++ index.js
@@ -0,0 +1,17 @@
+/*!
+ * bootprint-swagger <https://github.com/nknapp/bootprint-swagger>
+ *
+ * Copyright (c) 2016 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = bootprintSwagger
+/**
+ * Describe your module here
+ * @public
+ */
+function bootprintSwagger () {
+  // body
+}
---
+++ test/main-spec.js
@@ -0,0 +1,19 @@
+/*!
+ * bootprint-swagger <https://github.com/nknapp/bootprint-swagger>
+ *
+ * Copyright (c) 2016 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+/* global describe */
+// /* global it */
+// /* global xdescribe */
+// /* global xit */
+
+'use strict'
+
+var bootprintSwagger = require('../')
+
+describe('bootprint-swagger:', function () {
+  // body
+})
